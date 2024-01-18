b77d211747a059b85b5a73f0778bef2a24957369
Ryan Adi Prasetyo <ryanadiprasetyo2004@gmail.com>
2024-01-18 20:42:06 +0700

    initial commit

---
+++ src/app/components/Gallery.tsx
@@ -0,0 +1,24 @@
+import fetchImages from "@/lib/fetchImages";
+import type { ImagesResults } from "@/model/Images";
+
+export default async function Gallery() {
+
+    const url = 'https://api.pexels.com/v1/curated';
+
+    const images: ImagesResults | undefined = await fetchImages(url);
+    console.log(images);
+
+    if(!images) return <h2 className="m-4 text-2xl font-bold">No Images Found</h2>
+
+    
+
+    return (
+        <section>
+            <ul>
+                {images.photos.map(photo => (
+                    <li key={photo.id}>{photo.src.large}</li>
+                ))}
+            </ul>
+        </section>
+    )
+}
---
+++ src/lib/fetchImages.ts
@@ -0,0 +1,34 @@
+import type { ImagesResults } from "@/model/Images";
+import { ImagesSchemaWithPhotos } from "@/model/Images";
+import env from "./env";
+
+export default async function fetchImages(url: string): Promise<ImagesResults | undefined> {
+    try {
+        const res = await fetch(url, {
+            headers: {
+                Authorization: env.PEXELS_API_KEY
+            }
+        })
+
+        if (!res.ok) {
+            const errorResponse = await res.json();
+            console.log('Error response:', errorResponse);
+            throw new Error("Fetching images failed!\n");
+        }        
+
+        const imagesResults: ImagesResults = await res.json();
+
+        console.log(imagesResults);
+        
+        // Parse data with Zod schema
+        const parsedData = ImagesSchemaWithPhotos.parse(imagesResults)
+
+        if(parsedData.total_result === 0) return undefined;
+
+        return parsedData;
+
+    } catch (e) {
+        // Will show in terminal console
+        if(e instanceof Error) console.log(e.stack)
+    }
+}
