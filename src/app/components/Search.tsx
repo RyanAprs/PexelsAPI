93aef9433de7152afcce828e4ce33ebc26c8ccb2
Ryan Adi Prasetyo <ryanadiprasetyo2004@gmail.com>
2024-01-24 20:52:49 +0700

    add: navbar and search image

---
+++ src/app/components/Search.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import { useState, FormEvent } from "react"
+import { useRouter } from "next/navigation"
+
+export default function Search() {
+    const [search, setSearch] = useState('')
+    const router = useRouter()
+
+    const handleSearch = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        router.push(`/results/${search}`)
+        setSearch('')
+    }
+
+    return (
+        <form className="flex justify-center md:justify-between gap-2" onSubmit={handleSearch}>
+          <input
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Search"
+            className="bg-white p-2 w-[260px] sm:w-80 text-xl rounded-xl text-black"
+          />
+          <button
+            type="submit" 
+            className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-xl"
+          >
+            Search
+          </button>
+        </form>
+      );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import Navbar from './components/Navbar'
 
+export const revalidate = 3600
 
 export const metadata: Metadata = {
-  title: 'Image Gallery',
-  description: 'Image Gallery',
+  title: 'Ryan Image Gallery',
+  description: 'Image Gallery by Ryan',
 }
 
 export default function RootLayout({
@@ -15,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Navbar />
         <main className='max-w-6xl mx-auto'>
             {children}
         </main>
